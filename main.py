45fa540706c87c01329ec36c179de72b185901d1
Parthsa23 <sarswatparth55@gmail.com>
2022-12-28 18:33:58 +0530

    added issue functionality

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import datetime
+# import date and time to get the current date and time 
+from datetime import datetime,date,time
+
+
 class Library():
+
     def __init__(self):
         # self.pin=pin
+        self.lines=['1 Harry POtter','2 HOney sINGH','3 aJAY YADAV','4 PARTH SARASWAT','5 RISHABH SHARMA','6 GAURAV RAY']
         self.menu()
-
+# ['1 Harry POtter','2 HOney sINGH','3 aJAY YADAV',
+            #    '4 PARTH SARASWAT','5 RISHABH SHARMA','6 GAURAV RAY']
     def menu(self):
         user_input=input("""----------------------------------------------                              
                     1.  Enter 1 To Display Book
@@ -12,43 +18,73 @@ class Library():
                     4.  Enter 4 To Add A Book
                     5.  Enter 5 To Remove A Book
                     6.  Enter 6 To Update A Book
-                    7.  Enter 7 To Add A Book
-                    8.  Enter 8 To Get Current Date
-                    9.  Enter 9 To Get Current Time
-                    10. Enter 10 To Exit
+                    7.  Enter 7 To Get Current Date
+                    8.  Enter 8 To Get Current Time
+                    9. Enter 9 To Exit
                     """)
         # print(user_input)
         # checking Condition with provided user input
         if user_input == "1":
-            self.display()
+            self.Display()
         elif user_input == "2":
-            print ("Issue A Book")
+            self.Issue()
         elif user_input == "3":
-            print ("Return A Book")
+            self.Return()
         elif user_input == "4":
-            print ("Add A Book")
+            self.AddBook()
         elif user_input == "5":
-            print ("Remove A Book")
+            self.RemoveBook()
         elif user_input == "6":
-            print ("Update A Book")
+            self.UpdateBook()
         elif user_input == "7":
-            print ("Add A Book")
+            self.get_date()
         elif user_input == "8":
-            self.get_date
-        elif user_input == "9":
-            self.get_time 
+            self.get_time()
         else:
             print("Good Bye")
         
 
-    def display(self):
-        with open('books.txt',"r") as f:
-            lines = f.readlines()
-            print(lines,end=" ")
-    
-    # def get_date(self):
-    #     d = datetime.datetime.now()
-    #     print (self.get_date)
-    # def get_time(self):
-    #     e = time.time.now()
-    #     print (self.e)
+    def Display(self):
+        # with open('books.txt',"r") as f:
+        #     lines = f.readlines()
+
+        print(self.lines)
+        print ()
+
+    def Issue(self):
+
+        # with open('books.txt',"r") as f:
+        #     lines = f.readlines()
+        #     print(lines)
+
+        print(self.lines)
+        print("Note : If Intrested  in any of the above give input the first char. of the above mentioned")
+        user_input=int(input("Enter the number : "))
+        q=user_input -1
+        ranges=self.lines[q][0]
+        w=int(ranges)
+        r=self.lines[w]  
+        self.lines.remove(r)
+        print("Books Available are ", self.lines, "as the user selected" , r ,"book  for issue")
+# ___________________________________________________
+
+        if  w == user_input:
+            print("Succeded")
+            print(self.lines)
+        else:
+            self.menu()
+            
+    def Return(self):
+        pass
+    def AddBook(self):
+        pass
+    def RemoveBook(self):
+        pass
+
+    def UpdateBook(self):
+        pass
+    def get_date(self):
+        # Returns the current local date
+        print(datetime.now())
+    def get_time(self):                          #Issue with printing only date and time seperately will check later
+         print(datetime.time.now)
