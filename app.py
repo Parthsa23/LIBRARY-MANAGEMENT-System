b06c2ec2e1ffde1571ef01eff603e4b439f8044e
Parthsa23 <sarswatparth55@gmail.com>
2022-12-30 17:36:37 +0530

    updated project with different classes for each method

--- app.py
+++ app.py
@@ -1,46 +1,119 @@
-# import mysql.connectors
-class Library():
+import datetime
+class Library:
     def __init__(self):
-        Pin=input("Enter the Pin : " )
+        self.pin = int(input("Enter  Pin :  "))
+        self.books = ['Harry Potter', 'Honey Singh', 'Ajay Yadav', 'Parth Saraswat', 'RISHABH SHARMA', 'GAURAV RAY']
+        self.Cpin = int(input("Confirm Pin :  "))
+        self.book_issued = ""
+        self.current_time = datetime.datetime.now()
+class Display(Library):
+    print("Displaying the Book")
+    def __init__(self):
+        Library.__init__(self)
+    def display(self):
 
-class Menu(Library):
-    def __init__(self,Pin):
-        self.confirm_pin()
-
-
-    def confirm_pin(self):
-        Confirm_Pin=input("Enter the Confirm Pin : ")
-        if Confirm_Pin==self.Pin:
-            user_input=int(input("""                              
-                    1.  Enter 1 To Display Book
-                    2.  Enter 2 To Issue A Book
-                    3.  Enter 3 To Return A Book
-                    4.  Enter 4 To Add A Book
-                    5.  Enter 5 To Remove A Book
-                    6.  Enter 6 To Update A Book
-                    7.  Enter 7 To Get Current Date
-                    8.  Enter 8 To Get Current Time
-                    9.  Enter 9 To Exit
-
-
-                    """))
-       
-        # checking Condition with provided user input
-        if user_input == 1:
-            self.Display()
-        elif user_input == 2:
-            self.Issue()
-        elif user_input == 3:
-            self.Return()
-        elif user_input == 4:
-            self.AddBook()
-        elif user_input == 5:
-            self.RemoveBook()
-        elif user_input == 6:
-            self.UpdateBook()
-        elif user_input == 7:
-            self.get_date()
-        elif user_input == 8:
-            self.get_time()
+        if self.pin == self.Cpin:
+            print("The Available book are available below ")
+            print(self.books)
         else:
-            print("Good Bye")
+            print("Wrong Pin")
+
+class Issue(Library):
+    print("Issuing the book")
+    def __init__(self):
+        Library.__init__(self)
+    def issue(self):
+
+        if self.pin == self.Cpin:
+            print(self.books)
+            print("Note : If Interested  in any of the above give input the book number  of the above mentioned")
+            user_input = int(input("Enter the number : "))
+            b = user_input - 1  # deducting one value as the index always starts with zero
+            book_issued = self.books[b]  # Selecting the book and storing it in a variable named r
+            # print(book_issued ,"yeh wali issue ki h")
+            a = self.books.index(book_issued)  # checking the index  value of book selected by user
+            self.books.remove(book_issued)  # removing the selected book with help of index
+            print("You have selected ", book_issued, "book  for issue")
+            if b == a:
+                print("Congratulation the ", book_issued, "Book Is Issued at", self.current_time)
+
+            else:
+                print("your method is failed and now redirecting back to start")
+        else:
+            print("Wrong Pin")
+class Return(Issue, Library):
+    print("Returning the book")
+    def __init__(self):
+        Library.__init__(self)
+        Issue.__init__(self)
+
+    def ret(self):
+        if self.pin == self.Cpin:
+            print(self.book_issued)
+            print(self.books)
+            print("Thanks for return the book at" , self.current_time)
+        else:
+            print("Wrong Pin")
+
+
+class AddBook(Library):
+    print("Adding new book to stock")
+    def __init__(self):
+        Library.__init__(self)
+
+    def add(self):
+        if self.pin == self.Cpin:
+            user_input = input("Add  the Book Details : - ")
+            self.books.append(user_input)
+            print(self.books)
+        else:
+            print("There is some issue with the method")
+
+class Remove(Library):
+    print("Removing the book from library")
+    def __init__(self):
+        Library.__init__(self)
+    def remove(self):
+        if self.pin == self.Cpin:
+            print(self.books)
+            remove_book=input("Enter the  name of the book you want to remove:  ")
+            self.books.remove(remove_book)
+            print ("Successfully Removed ")
+            print(self.books)
+        else:
+            print("There is some issue with the method")
+
+class Update(Library):
+    print("Updating the book  in library")
+    def __init__(self):
+        Library.__init__(self)
+    def update(self):
+        if self.pin == self.Cpin:
+            print(self.books)
+            book_index_number = int(input("Enter the book number  you want to update : "))
+            update1 = book_index_number-1
+            update2 = input("Enter the name of updated book  :    ")
+            self.books[update1] = update2
+            print("Hi There! The", self.books[update1], "is updated to", update2)
+            print(self.books)
+        else:
+            print("There is some issue with the pin you entered")
+
+
+
+
+# canara = Display()
+# canara.display()
+hdfc = Return()
+hdfc.ret()
+# sbi = Issue()
+# sbi.issue()
+# axis = AddBook()
+# axis.add()
+# cnbc = Remove()
+# cnbc.remove()
+# idbi = Update()
+# idbi.update()
+
+
+
--- display.py
+++
@@ -1,5 +0,0 @@
-class Display():
-    def __init__(self):
-        with open('books.txt') as f:
-            lines = f.readlines()
-            print(lines)
